c46da922a50fc67673ecfcbb549d81716d3976c3
sr-garje99 <shubham.garje198@gmail.com>
2024-01-06 22:59:47 +0530

    full screen map

--- src/components/Body.js
+++ src/components/Body.js
@@ -33,13 +33,13 @@ const Body = () => {
         </Card>
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={1}>
-                <Grid item xs={12} sm={12} md={4} lg ={3} xl={3} >
+                {/* <Grid item xs={12} sm={12} md={4} lg ={3} xl={3} >
                     <Card sx={{height:"16rem",padding:"1rem",margin:"0.2rem",border:"1rem"}}>
                         <Form/>
                     </Card>    
-                </Grid>
+                </Grid> */}
                 <Grid item xs={12} sm={12} md={12} lg ={12} xl={12} >
-                    <Card sx={{height:"30rem",padding:"1rem",margin:"0.2rem",border:"1rem"}}>
+                    <Card sx={{height:"100rem",padding:"1rem",margin:"0.2rem",border:"1rem"}}>
                         <MapBox position={position}/>
                     </Card>
                 </Grid>
